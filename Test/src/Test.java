5e1775f392647d2b04ed57933711c6b201362fb2
Jin\User <hate7354@naver.com>
2021-03-23 15:13:33 +0900

    # WARNING: head commit changed in the meantime
    Change 생성된 프로그램입니당

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("이미 사라진 테스트입니다.");
+		System.out.println("생성된 프로그램입니다.");
 	}
 
 }
